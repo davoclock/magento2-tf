87b861f4dfbb148380606e0f310e52449880a90b
David D <ddiaz@anthementertainment>
2020-07-20 21:44:58 -0400

    Bastion Host added

---
+++ modules/ec2/inputs.tf
@@ -0,0 +1,4 @@
+variable "ssh_key" {}
+variable "bastion_host_ami" {}
+variable "bastion_host_sg" {}
+variable "subnet_id" {}
---
+++ modules/ec2/main.tf
@@ -0,0 +1,34 @@
+resource "aws_key_pair" "bastion_key" {
+  key_name   = "bastion_key"
+  public_key = var.ssh_key
+}
+
+data "aws_ami" "al2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.20200617.0-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners =["137112412989"]
+}
+
+
+resource "aws_instance" "bastion_host" {
+  ami           = data.aws_ami.al2.id
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.bastion_key.key_name
+  security_groups = [var.bastion_host_sg]
+  subnet_id       = var.subnet_id
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "Bastion Host"
+  }
+}
